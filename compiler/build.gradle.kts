37af93d768908794ffd2c01ee5259fb56bc56311
Pavel Strelchenko <p.strelchenko@hh.ru>
2023-03-02 17:24:52 +0600

    Добавляет gradle-таску для сбора fatJar-а

--- compiler/build.gradle.kts
+++ compiler/build.gradle.kts
@@ -21,42 +21,10 @@ dependencies {
     testImplementation(projects.compilerTest)
 }
 
-val fatJar = task("fatJar", type = Jar::class) {
-//    baseName = "${project.name}-fat"
-//    manifest {
-//        attributes["Implementation-Title"] = "Gradle Jar File Example"
-//        attributes["Implementation-Version"] = version
-//        attributes["Main-Class"] = "com.mkyong.DateUtils"
-//    }
-    from(configurations.runtimeClasspath.get().map({ if (it.isDirectory) it else zipTree(it) }))
+tasks.register("fatJar", type = Jar::class) {
+    val directories = configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) }
+
+    from(directories)
+    duplicatesStrategy = org.gradle.api.file.DuplicatesStrategy.INCLUDE
     with(tasks.jar.get() as CopySpec)
 }
-
-//tasks.named("build") { dependsOn(fatJar) }
-
-//tasks {
-//    "build" {
-//        dependsOn(fatJar)
-//    }
-//}
-
-//kotlin {
-//    jvm {
-//        compilations {
-//            val main = getByName("main")
-//            tasks {
-//                register<Jar>("fatJar") {
-//                    group = "application"
-//                    manifest {
-//                        attributes["Implementation-Title"] = "Gradle Jar File Example"
-//                        attributes["Implementation-Version"] = archiveVersion
-//                        attributes["Main-Class"] = "[[mainClassPath]]"
-//                    }
-//                    archiveBaseName.set("${project.name}-fat")
-//                    from(main.output.classesDirs, main.compileDependencyFiles)
-//                    with(jar.get() as CopySpec)
-//                }
-//            }
-//        }
-//    }
-//}
